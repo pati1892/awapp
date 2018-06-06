2028e40d5291ac4ffb61ed1521ba202384fbb4e6
Nils Rudolph <nils.rudolph@student.fhws.de>
2018-06-06 11:23:25 +0200

    prepare application web service

---
+++ NAA/NAA.WebService/ApplicationService.asmx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace NAA.WebService
+{
+    /// <summary>
+    /// Summary description for ApplicationService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class ApplicationService : System.Web.Services.WebService
+    {
+
+
+    }
+}
