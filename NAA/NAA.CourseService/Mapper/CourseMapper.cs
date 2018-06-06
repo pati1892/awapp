9a3a7160ce2d9708018b48585cba247201030fc8
Nils Rudolph <nils.rudolph@student.fhws.de>
2018-06-06 14:33:06 +0200

    implement service to get courses of universities

---
+++ NAA/NAA.CourseService/Mapper/CourseMapper.cs
@@ -0,0 +1,54 @@
+﻿using NAA.CourseService.Bean;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NAA.CourseService.Mapper
+{
+    internal class CourseMapper
+    {
+        #region Fields
+
+        private static CourseMapper instance = null;
+
+        #endregion Fields
+
+        #region Constructors
+
+        private CourseMapper()
+        {
+
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        public static CourseMapper GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new CourseMapper();
+            }
+            return instance;
+        }
+
+        public CourseBEAN Map(Sheffield.Course course)
+        {
+            var result = new CourseBEAN();
+            
+            return result;
+        }
+        public CourseBEAN Map(SheffieldHallam.SHUCourse course)
+        {
+            var result = new CourseBEAN();
+
+            return result;
+        }
+
+        #endregion Methods
+
+    }
+}
---
+++ NAA/NAA.CourseService/Service/CourseService.cs
@@ -0,0 +1,50 @@
+﻿using NAA.CourseService.Bean;
+using NAA.CourseService.IService;
+using NAA.CourseService.Mapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NAA.CourseService.Service
+{
+    public class CourseService : ICourseService
+    {
+        private readonly Sheffield.SheffieldWebService sheffieldProxy = null;
+        private readonly SheffieldHallam.SHUWebService sheffieldHalamProxy = null;
+        public CourseService()
+        {
+            sheffieldProxy = new Sheffield.SheffieldWebService();
+            sheffieldHalamProxy = new SheffieldHallam.SHUWebService();
+        }
+
+        public IList<CourseBEAN> GetSheffieldCourses()
+        {
+            var courseList = sheffieldProxy.SheffCourses();
+            IList<CourseBEAN> result = new List<CourseBEAN>();
+            if (courseList != null)
+            {
+                foreach (Sheffield.Course course in courseList)
+                {
+                    result.Add(CourseMapper.GetInstance().Map(course));
+                }
+            }
+            return result;
+        }
+
+        public IList<CourseBEAN> GetSheffieldHallamCourses()
+        {
+            var courseList = sheffieldHalamProxy.SHUCourses();
+            IList<CourseBEAN> result = new List<CourseBEAN>();
+            if (courseList != null)
+            {
+                foreach (SheffieldHallam.SHUCourse course in courseList)
+                {
+                    result.Add(CourseMapper.GetInstance().Map(course));
+                }
+            }
+            return result;
+        }
+    }
+}
