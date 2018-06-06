98ccbcc51fb226a529eb168347eda487511e33ae
Nils Rudolph <nils.rudolph@student.fhws.de>
2018-06-06 14:55:56 +0200

    implment Webservice ApplicationService

--- NAA/NAA.WebService/ApplicationService.asmx.cs
+++ NAA/NAA.WebService/ApplicationService.asmx.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using naass = NAA.Service.Service;
+using naasi = NAA.Service.IService;
+using NAA.Data.Enum;
 
 namespace NAA.WebService
 {
@@ -17,29 +20,73 @@ namespace NAA.WebService
     // [System.Web.Script.Services.ScriptService]
     public class ApplicationService : System.Web.Services.WebService
     {
+        private readonly naasi.IApplicationService appService = null;
+
+        public ApplicationService()
+        {
+            appService = new naass.ApplicationService();
+        }
 
         #region Methods
 
-        [WebMethod]
+        [WebMethod(Description ="Get all Applications of an university")]
         public List<ApplicationBEAN> GetApplications(string universityName)
         {
-            return null;
+            return appService.GetApplications(universityName).ToList();
         }
 
-        [WebMethod]
+        [WebMethod(Description ="Updates the state of an application to 'conditonal'")]
         public bool SetConditionalApplication(int applicationId)
         {
+            var application = appService.GetApplication(applicationId);
+            var currentValue = application.UniversityOffer;
+            var newValue = ((char)ApplicationState.Conditional).ToString();
+            if (currentValue != newValue)
+            {
+                if (application.Firm.HasValue && application.Firm.Value || currentValue == ((char)ApplicationState.Reject).ToString() || currentValue == ((char)ApplicationState.Unconditional).ToString())
+                {
+                    return false;
+                }
+            }
+            application.UniversityOffer = newValue;
+            appService.EditApplication(application);
             return true;
         }
 
-        [WebMethod]
+        [WebMethod(Description = "Updates the state of an application to 'reject'")]
         public bool SetRejectApplication(int applicationId)
         {
+            var application = appService.GetApplication(applicationId);
+            var currentValue = application.UniversityOffer;
+            var newValue = ((char)ApplicationState.Reject).ToString();
+            if (currentValue != newValue)
+            {
+                if (application.Firm.HasValue && application.Firm.Value || currentValue != ((char)ApplicationState.Pending).ToString())
+                {
+                    return false;
+                }
+            }
+            application.UniversityOffer = newValue;
+            appService.EditApplication(application);
             return true;
         }
-        [WebMethod]
+
+
+        [WebMethod(Description = "Updates the state of an application to 'unconditional'")]
         public bool SetUnconditionalApplication(int applicationId)
         {
+            var application = appService.GetApplication(applicationId);
+            var currentValue = application.UniversityOffer;
+            var newValue = ((char)ApplicationState.Reject).ToString();
+            if (currentValue != newValue)
+            {
+                if (application.Firm.HasValue && application.Firm.Value || currentValue == ((char)ApplicationState.Reject).ToString())
+                {
+                    return false;
+                }
+            }
+            application.UniversityOffer = newValue;
+            appService.EditApplication(application);
             return true;
         }
 
