e80e0be8cf3a4b3faeb430034e5dd4e09afe4b3b
Nils Rudolph <nils.rudolph@student.fhws.de>
2018-06-08 12:07:36 +0200

    add init database script improve application design

---
+++ infos/init-database.sql
@@ -0,0 +1,6 @@
+TRUNCATE TABLE Applicant;
+TRUNCATE TABLE [Application];
+TRUNCATE TABLE University;
+
+INSERT INTO University(UniversityName) VALUES ('Sheffield Hallam');
+INSERT INTO University(UniversityName) VALUES ('Sheffield');
